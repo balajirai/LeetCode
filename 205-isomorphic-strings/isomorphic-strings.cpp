9a72aece55bf9d160c77c2739869801ee1901254
Balaji <97026438+balajirai@users.noreply.github.com>
2024-04-02 10:51:40 +0530

    Time: 11 ms (15.86%) | Memory: 9.3 MB (42.66%) - Balaji

--- 205-isomorphic-strings/isomorphic-strings.cpp
+++ 205-isomorphic-strings/isomorphic-strings.cpp
@@ -1,13 +1,22 @@
 class Solution {
 public:
     bool isIsomorphic(string s, string t) {
-        int m1[256] = {0};
-        int m2[256] = {0};
-        int n = s.size();
-        for (int i = 0; i < n; ++i) {
-            if (m1[s[i]] != m2[t[i]]) return false;
-            m1[s[i]] = i + 1;
-            m2[t[i]] = i + 1;
+        unordered_map<char,char>mp1;
+        unordered_map<char,char>mp2;
+
+        for(int i=0; i<s.size(); i++){
+            char ch1 = s[i];
+            char ch2 = t[i];
+
+            if(mp1.find(ch1) != mp1.end() && mp1[ch1] != ch2){
+                return false;
+            }
+            else if(mp2.find(ch2) != mp2.end() && mp2[ch2] != ch1){
+                return false;
+            }
+
+            mp1[ch1] = ch2;
+            mp2[ch2] = ch1;
         }
         return true;
     }
