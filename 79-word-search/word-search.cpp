99732b5371eb12daa510f91b4d0423f75c62f66e
Balaji <97026438+balajirai@users.noreply.github.com>
2024-04-03 23:43:49 +0530

    Time: 249 ms (75.40%) | Memory: 9.8 MB (72.58%) - Balaji

--- 79-word-search/word-search.cpp
+++ 79-word-search/word-search.cpp
@@ -1,98 +1,40 @@
 class Solution {
 private:
-    bool ans = false;
-    bool isSafe(vector<vector<char>>& board, string &word, int x, int y, int m, int n, int idx){
-        if((x>=0 && y>=0) && (x<m && y<n) && (board[x][y]==word[idx])) return true;
-        else return false;
-    }
-    void solve(vector<vector<char>>& board, string output, int x, int y, int m, int n, int idx, string &word){
-        // base case
-        if(output == word){
-            ans = true;
-            return;
+    int m, n;
+    vector<vector<int>>directions{{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+
+    int find(vector<vector<char>>& board, int i, int j, int index, string &word){
+        if(index == word.size()) return true;
+
+        if(i < 0 || j < 0 || i >= m || j >= n) return false;
+        if(board[i][j] != word[index]) return false;
+
+        char temp = board[i][j];
+        board[i][j] = '$';
+
+        for(auto &dir:directions){
+            int new_i = i + dir[0];
+            int new_j = j + dir[1];
+
+            if(find(board, new_i, new_j, index+1, word)) return true;
         }
 
-        if(idx >= word.size() || x >= m || y >= n){
-            return;
-        }
-
-        // 4 choices Top right bottom left
-
-        // TOP
-        int newx = x-1;
-        int newy = y;
-
-        if(isSafe(board, word, newx, newy, m, n, idx)){
-            char c = board[newx][newy];     
-            output.push_back(c);
-            board[newx][newy] = '#';   // visited
-            solve(board, output, newx, newy, m, n, idx+1, word);
-            output.pop_back();    // backtrack
-            board[newx][newy] = c;
-            
-        }
-
-        // RIGHT
-        newx = x;
-        newy = y+1;
-
-        if(isSafe(board, word, newx, newy, m, n, idx)){
-            char c = board[newx][newy];     
-            output.push_back(c);
-            board[newx][newy] = '#';   // visited
-            solve(board, output, newx, newy, m, n, idx+1, word);
-            output.pop_back();         // backtrack
-            board[newx][newy] = c;
-            
-        }
-
-        // BOTTOM
-        newx = x+1;
-        newy = y;
-
-        if(isSafe(board, word, newx, newy, m, n, idx)){
-            char c = board[newx][newy];     
-            output.push_back(c);
-            board[newx][newy] = '#';   // visited
-            solve(board, output, newx, newy, m, n, idx+1, word);
-            output.pop_back();    // backtrack
-            board[newx][newy] = c;
-        }
-
-        // LEFT
-        newx = x;
-        newy = y-1;
-
-        if(isSafe(board, word, newx, newy, m, n, idx)){
-            char c = board[newx][newy];     
-            output.push_back(c);
-            board[newx][newy] = '#';   // visited
-            solve(board, output, newx, newy, m, n, idx+1, word);
-            output.pop_back();    // backtrack
-            board[newx][newy] = c;
-        }
+        board[i][j] = temp;
+        
+        return false;
     }
 public:
     bool exist(vector<vector<char>>& board, string word) {
-        int m = board.size();
-        int n = board[0].size();
-        string output;
+        m = board.size();
+        n = board[0].size();
 
         for(int i=0; i<m; i++){
             for(int j=0; j<n; j++){
-                if(board[i][j]==word[0]){
-                    int srcx = i;
-                    int srcy = j;
-                    int idx = 0;
-                    output.push_back(word[0]);
-                    board[i][j] = '#'; 
-                    solve(board, output, srcx, srcy, m, n, idx+1, word);
-                    output.pop_back();
-                    board[i][j] = word[0]; 
+                if(board[i][j] == word[0] && find(board, i, j, 0, word)){
+                    return true;
                 }
             }
         }
-
-        return ans;
+        return false;
     }
 };
