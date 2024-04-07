4fbd8377d3d8a3081faf2f377a235b5968500cc0
Balaji <97026438+balajirai@users.noreply.github.com>
2024-04-07 16:22:12 +0530

    Time: 0 ms (100.00%) | Memory: 9.5 MB (7.00%) - Balaji

--- 678-valid-parenthesis-string/valid-parenthesis-string.cpp
+++ 678-valid-parenthesis-string/valid-parenthesis-string.cpp
@@ -1,21 +1,40 @@
 class Solution {
+private:
+    bool solve(int index, int open, string &s, vector<vector<int>>&dp){
+        if(index == s.size()) return open == 0;
+
+        if(dp[index][open] != -1) return dp[index][open];
+
+        bool isValid = false;
+
+        // case 1 : '*'
+        if(s[index] == '*'){
+            // option 1 : open
+            isValid |= solve(index+1, open+1, s, dp);
+
+            // option 2 : empty ""
+            isValid |= solve(index+1, open, s, dp);
+
+            // option 3 : close
+            if(open > 0){
+                isValid |= solve(index+1, open-1, s, dp);
+            }
+        }
+        // case 2 : '('
+        else if(s[index] == '('){
+            isValid |= solve(index+1, open+1, s, dp);
+        }
+        // case 3 : ')'
+        else if(open > 0){
+            isValid |= solve(index+1, open-1, s, dp);
+        }
+
+        return dp[index][open] = isValid;
+    }
 public:
     bool checkValidString(string s) {
-        int leftMin = 0, leftMax = 0;
-
-        for (char &c : s) {
-            if (c == '(') {
-                leftMin++, leftMax++;
-            } 
-            else if (c == ')') {
-                leftMin--, leftMax--;
-            } 
-            else {
-                leftMin--, leftMax++;
-            }
-            if (leftMax < 0) return false;
-            if (leftMin < 0) leftMin = 0;
-        }
-        return leftMin == 0;
+        int n = s.size();
+        vector<vector<int>>dp(n+1, vector<int>(n+1, -1));
+        return solve(0, 0, s, dp);
     }
 };
