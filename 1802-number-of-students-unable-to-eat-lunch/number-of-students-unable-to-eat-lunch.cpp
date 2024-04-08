0986d51a68424e10b005e177f931ad39cab2912b
Balaji <97026438+balajirai@users.noreply.github.com>
2024-04-08 08:50:01 +0530

    Time: 5 ms (31.29%) | Memory: 10.7 MB (79.46%) - Balaji

---
+++ 1802-number-of-students-unable-to-eat-lunch/number-of-students-unable-to-eat-lunch.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int countStudents(vector<int>& students, vector<int>& sandwiches) {
+        int circular = 0, square = 0;
+        
+        for(int i=0; i<students.size(); i++){
+            if(students[i] == 0) circular++;
+            else square++;
+        }
+
+        for(int i=0; i<sandwiches.size(); i++){
+            if(sandwiches[i] == 0){
+                if(circular > 0) circular--;
+                else return square;
+            }
+            else{
+                if(square > 0) square--;
+                else return circular;
+            }
+        }
+        return 0;
+    }
+};
