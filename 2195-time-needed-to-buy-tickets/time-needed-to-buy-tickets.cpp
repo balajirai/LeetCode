99c1cb1efd22be04aaf52ce6aac6e7d86bd52246
Balaji <97026438+balajirai@users.noreply.github.com>
2024-04-09 10:38:59 +0530

    Time: 0 ms (100.00%) | Memory: 9.4 MB (67.56%) - Balaji

--- 2195-time-needed-to-buy-tickets/time-needed-to-buy-tickets.cpp
+++ 2195-time-needed-to-buy-tickets/time-needed-to-buy-tickets.cpp
@@ -1,23 +1,11 @@
 class Solution {
 public:
     int timeRequiredToBuy(vector<int>& tickets, int k) {
-        deque<int>dq;
-        for(int i=0; i<tickets.size(); i++){
-            dq.push_back(i);
-        }
         int time = 0;
-        while(!dq.empty()){
-            int frontIdx = dq.front();
-            dq.pop_front();
-            if(tickets[frontIdx]){
-                time++;
-                tickets[frontIdx]--;
-                if(tickets[frontIdx]) dq.push_back(frontIdx);
-            }
-            if(frontIdx == k && tickets[frontIdx] == 0){
-                return time;
-            }
+        for(int i=0; i<tickets.size(); i++){
+            if(i<=k) time += min(tickets[k], tickets[i]);
+            else time += min(tickets[k]-1, tickets[i]);
         }
-        return 0;
+        return time;
     }
 };
