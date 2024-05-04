1ed31ac8d5477e61f7a51f97d35545a25ded3c4a
Balaji <97026438+balajirai@users.noreply.github.com>
2024-05-04 10:17:54 +0530

    Time: 66 ms (13.06%) | Memory: 45.5 MB (46.86%)

--- 917-boats-to-save-people/boats-to-save-people.cpp
+++ 917-boats-to-save-people/boats-to-save-people.cpp
@@ -4,19 +4,11 @@ public:
         sort(people.begin(), people.end());
         int start = 0, end = people.size()-1, ans = 0;
         while(start <= end){
-            if(start == end){
-                ans++;
-                break;
-            }
-            else if(people[end] + people[start] <= limit){
-                ans++;
+            if(people[end] + people[start] <= limit){
                 start++, end--;
             }
-            else{
-                ans++;
-                end--;
-            }
-
+            else  end--;
+            ans++;
         }
         return ans;
     }
