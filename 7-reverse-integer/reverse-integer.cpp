0e837d3d79d8c484dac786356f8b3664897161d8
Balaji <97026438+balajirai@users.noreply.github.com>
2024-05-07 00:26:37 +0530

    Time: 0 ms (100.00%) | Memory: 7.3 MB (98.64%)

--- 7-reverse-integer/reverse-integer.cpp
+++ 7-reverse-integer/reverse-integer.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int reverse(int n) {
         int rev = 0;
-        while(n != 0){
+        while(n){
             if((rev > INT_MAX/10) or rev < INT_MIN/10) return 0;
             int digit = n%10;
             n /= 10;
