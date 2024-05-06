3d2a4b5b36aaaa0be9710bb2143fa3439ad67752
Balaji <97026438+balajirai@users.noreply.github.com>
2024-05-06 14:35:57 +0530

    Time: 356 ms (5.00%) | Memory: 170.8 MB (29.07%)

--- 2573-remove-nodes-from-linked-list/remove-nodes-from-linked-list.cpp
+++ 2573-remove-nodes-from-linked-list/remove-nodes-from-linked-list.cpp
@@ -1,29 +1,15 @@
 class Solution {
 public:
     ListNode* removeNodes(ListNode* head) {
-        stack<ListNode*>st;
-        ListNode* curr = head;
-        while(curr){
-            st.push(curr);
-            curr = curr->next;
+        if(!head || !head->next) return head;
+        ListNode* nextNode = removeNodes(head->next);
+
+        if(head->val < nextNode->val){
+            delete head;
+            return nextNode;
         }
 
-        curr = st.top();
-        st.pop();
-        int maxNode = curr->val;
-        ListNode* resultHead = new ListNode(curr->val);
-
-        while(!st.empty()){
-            curr = st.top();
-            st.pop();
-            if(curr->val >= maxNode){
-                ListNode* newNode = new ListNode(curr->val);
-                newNode->next = resultHead;
-                resultHead = newNode;
-                maxNode = curr->val;
-            }
-        }
-        
-        return resultHead;
+        head->next = nextNode;
+        return head;
     }
 };
