19d39e32b3d18100058e78fb5138468da8457f51
Balaji <97026438+balajirai@users.noreply.github.com>
2024-05-20 07:25:43 +0530

    Time: 4 ms (55.14%) | Memory: 8.4 MB (65.34%)

---
+++ 1993-sum-of-all-subset-xor-totals/sum-of-all-subset-xor-totals.cpp
@@ -0,0 +1,13 @@
+class Solution {
+private:
+    int helper(vector<int>& nums, int level, int currentXOR) {
+        if (level == nums.size()) return currentXOR;
+        int include = helper(nums, level + 1, currentXOR ^ nums[level]);
+        int exclude = helper(nums, level + 1, currentXOR);
+        return include + exclude;
+    }
+public:
+    int subsetXORSum(vector<int>& nums) {
+        return helper(nums, 0, 0);
+    }
+};
