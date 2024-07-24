223e24c329aeab444fb7affa03f3031eeed5f016
Balaji <97026438+balajirai@users.noreply.github.com>
2024-07-24 22:09:24 +0530

    Time: 628 ms (20.35%) | Memory: 90.8 MB (46.75%)

---
+++ 1333-sort-the-jumbled-numbers/sort-the-jumbled-numbers.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<int> sortJumbled(vector<int>& mapping, vector<int>& nums) {
+        vector<pair<int,int>>temp;
+        for(int &num:nums){
+            string str1 = to_string(num), str2 = "";
+            for(char &ch:str1){
+                int ind = ch - '0';
+                int val = mapping[ind];
+                str2 += to_string(val);
+            }
+            int newNum = stoi(str2);
+            temp.push_back({newNum, num});
+        }
+        auto comparator = [](const pair<int, int>& a, const pair<int, int>& b) {
+            return a.first < b.first;
+        };
+
+        stable_sort(temp.begin(), temp.end(), comparator);
+
+        vector<int>ans;
+        for(auto &p:temp){
+            cout << p.first << " -> " << p.second << endl;
+            ans.push_back(p.second);
+        }
+        return ans;
+    }
+};
