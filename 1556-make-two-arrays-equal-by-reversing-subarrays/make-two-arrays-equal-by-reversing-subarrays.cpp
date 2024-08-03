ddf4066c887064ffb95d6d511585e572a2928ec7
Balaji <97026438+balajirai@users.noreply.github.com>
2024-08-03 13:50:40 +0530

    Time: 11 ms (43.49%) | Memory: 17.6 MB (42.71%)

--- 1556-make-two-arrays-equal-by-reversing-subarrays/make-two-arrays-equal-by-reversing-subarrays.cpp
+++ 1556-make-two-arrays-equal-by-reversing-subarrays/make-two-arrays-equal-by-reversing-subarrays.cpp
@@ -1,12 +1,10 @@
 class Solution {
 public:
     bool canBeEqual(vector<int>& target, vector<int>& arr) {
-        if(target.size()!= arr.size()) return false;
-        unordered_map<int,int>mp;
-        for(int &num:target) mp[num]++;
-        for(int &num:arr) mp[num]--;
-        for(auto &num:mp){
-            if(mp[num.first] > 0) return false;
+        sort(target.begin(), target.end());
+        sort(arr.begin(), arr.end());
+        for(int i=0; i<target.size(); i++){
+            if(target[i] != arr[i]) return false;
         }
         return true;
     }
