8add6d749fe6f6859cb12d0dc52c19cfec19f02a
Balaji <97026438+balajirai@users.noreply.github.com>
2024-08-04 16:56:50 +0530

    Time: 94 ms (45.62%) | Memory: 27.5 MB (17.08%)

--- 1615-range-sum-of-sorted-subarray-sums/range-sum-of-sorted-subarray-sums.cpp
+++ 1615-range-sum-of-sorted-subarray-sums/range-sum-of-sorted-subarray-sums.cpp
@@ -1,21 +1,26 @@
 class Solution {
 public:
     int rangeSum(vector<int>& nums, int n, int left, int right) {
-        // better
-        vector<int>sums;
-        for (int start = 0; start < n; ++start){
-            int sum = 0;
-            for (int end = start; end < n; ++end){
-                sum += nums[end];
-                sums.push_back(sum);
+        vector<int>subarraySums, prefixSums(n + 1, 0);
+        int MOD = 1e9 + 7;
+
+        for (int i = 0; i < n; ++i) {
+            prefixSums[i + 1] = prefixSums[i] + nums[i];
+        }
+
+        for (int start = 0; start < n; ++start) {
+            for (int end = start; end < n; ++end) {
+                subarraySums.push_back(prefixSums[end + 1] - prefixSums[start]);
             }
         }
-        sort(sums.begin(), sums.end());
-        left = left-1, right = right-1;
-        int ans = 0, MOD = 1e9+7;
-        for(int i=left; i<=right; i++){
-            ans = (ans + sums[i]) % MOD;
+        sort(subarraySums.begin(), subarraySums.end());
+
+        int result = 0;
+        left -= 1, right -= 1;
+        for (int i = left; i <= right; ++i) {
+            result = (result + subarraySums[i]) % MOD;
         }
-        return ans;
+
+        return result;
     }
 };
