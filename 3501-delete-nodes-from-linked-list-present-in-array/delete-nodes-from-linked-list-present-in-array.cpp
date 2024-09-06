4436770454cd87b1d05745d3180a63097577deca
Balaji <97026438+balajirai@users.noreply.github.com>
2024-09-06 23:18:43 +0530

    Time: 449 ms (51.55%) | Memory: 272.2 MB (23.14%)

---
+++ 3501-delete-nodes-from-linked-list-present-in-array/delete-nodes-from-linked-list-present-in-array.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    ListNode* modifiedList(vector<int>& nums, ListNode* head) {
+        unordered_map<int,int>mp;
+        for(auto i:nums) mp[i]++;
+        ListNode *curr=head, *prev=head;
+
+        while(curr!=nullptr){
+            if(mp[curr->val]>0){
+                if(prev==curr){
+                    if(head==curr) head=head->next;
+                    curr=curr->next;
+                    prev=curr;
+                }
+                else{
+                    curr=curr->next;
+                    prev->next=curr;
+                }
+            }
+            else {
+                prev=curr;
+                curr=curr->next;
+            }
+        }
+        return head;
+    }
+};
