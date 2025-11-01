3cd5f1e52da93e9aa76c8395ca43b68c54fee581
Balaji <97026438+balajirai@users.noreply.github.com>
2025-11-01 15:22:47 +0530

    Time: 51 ms (54.78%) | Memory: 90.7 MB (68.91%)

---
+++ 217-contains-duplicate/contains-duplicate.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        // Using Hashmap
+        unordered_map<int,int>hash;
+        for(int i=0; i<nums.size(); i++){
+            hash[nums[i]]++;
+            if(hash[nums[i]] > 1) return true;
+        }
+        return false;
+    }
+};
