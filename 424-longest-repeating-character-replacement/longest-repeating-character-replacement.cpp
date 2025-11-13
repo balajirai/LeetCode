839b6d1991a8e30993e8ecfbacfcbae8d1adcfa9
Balaji <97026438+balajirai@users.noreply.github.com>
2025-11-14 01:18:01 +0530

    Time: 0 ms (100.00%) | Memory: 10.7 MB (92.15%)

---
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int characterReplacement(string s, int k) {
+        // Sliding Window
+        vector<int>hash(26,0);
+        int maxLen = 0, maxFreq = 0;
+        int left = 0, right = 0, n = s.size();
+        while(right < n){
+            hash[s[right]-'A']++;
+            maxFreq = max(maxFreq, hash[s[right]-'A']);
+            while((right-left+1) - maxFreq > k){
+                hash[s[left]-'A']--;
+                left++;
+            }
+            if((right-left+1) - maxFreq <= k){
+                maxLen = max(maxLen, (right-left+1));
+            }
+            right++;
+        }
+        return maxLen;
+    }
+};
